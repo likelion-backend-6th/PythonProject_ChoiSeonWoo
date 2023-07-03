3cea1f74d232cda1dbd831d8c7a09b5640f0b759
25th-Night <brown_eyed87@naver.com>
2023-07-04 02:20:12 +0900

    Users 앱 - utils.py : sign_up 구현

--- users/utils.py
+++ users/utils.py
@@ -1,9 +1,55 @@
+import re
 from time import sleep
 
 from users.models import Users
 
 my_info = {"user": None, "is_logined": False}
 
+
+
+def sign_up():
+    usr_cnt, pw_cnt = 0, 0
+
+    users = Users().get()
+
+    username = input("사용자명을 입력해주세요. : ")
+    username_check = map(lambda x: x[1] == username, users)
+    while True in username_check:
+        usr_cnt += 1
+        username = input(f"이미 가입된 정보가 있습니다. ({usr_cnt}/3)  사용자명을 다시 입력해주세요. : ")
+        username_check = map(lambda x: x[1] == username, users)
+        if usr_cnt == 3:
+            print("3회 이상 실패하였으므로 초기 메뉴로 돌아갑니다.")
+            sleep(0.5)
+            for i in range(3):
+                print(3 - i)
+                sleep(0.5)
+            sign_up()
+
+    fullname = input("성함을 입력해주세요. : ")
+
+    password = input("비밀번호를 입력해주세요. : ")
+
+    match = "^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$"
+    validation = re.compile(match)
+
+    while validation.match(str(password)) == None:
+        pw_cnt += 1
+        password = input(f"비밀번호는 하나 이상의 문자, 숫자, 특수문자를 포함하여 8자리 이상으로 작성해주세요. ({pw_cnt}/3)  : ")
+        if pw_cnt == 3:
+            print("3회 이상 실패하였으므로 초기 메뉴로 돌아갑니다.")
+            sleep(0.5)
+            for i in range(3):
+                print(3 - i)
+                sleep(0.5)
+            sign_up()
+
+    new_user = Users(username, fullname, password)
+    new_user.post()
+
+    print("회원가입이 완료되었습니다.")
+    print("이어서 로그인을 바로 진행하도록 하겠습니다.")
+    return login()
 
 def login() -> object:
     usr_cnt, pw_cnt = 0, 0
@@ -48,9 +94,12 @@ def logout() -> object:
     return my_info
 
 
-login()
+# login()
+#
+# if input("로그아웃 하시겠습니까? : ") == "예":
+#     logout()
+#
+# print(f"my_info: {my_info}")
 
-if input("로그아웃 하시겠습니까? : ") == "예":
-    logout()
 
-print(f"my_info: {my_info}")
+sign_up()
