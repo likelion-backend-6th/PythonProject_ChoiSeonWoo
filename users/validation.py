3ec977fd0d8fb222e8d0fd058da695ed1ff61232
25th-Night <brown_eyed87@naver.com>
2023-07-04 09:15:59 +0900

    Users 앱 - validation.py : 비밀번호 유효성 검사를 별도 함수로 분리

--- users/validation.py
+++ users/validation.py
@@ -18,3 +18,23 @@ def username_validation(username: str, users: List):
                 sleep(0.5)
             return False  # 중복 검사 실패
         username = input(f"이미 가입된 정보가 있습니다. ({usr_cnt}/3)  사용자명을 다시 입력해주세요. : ")
+
+
+def password_validation(password: str, users: List):
+    pw_cnt = 0
+
+    match = "^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$"
+    validation = re.compile(match)
+
+    while True:
+        if validation.match(password) is not None:
+            return True
+        pw_cnt += 1
+        if pw_cnt == 3:
+            print("3회 이상 실패하였으므로 초기 메뉴로 돌아갑니다.")
+            sleep(0.5)
+            for i in range(3):
+                print(3 - i)
+                sleep(0.5)
+            return False
+        password = input(f"비밀번호는 하나 이상의 문자, 숫자, 특수문자를 포함하여 8자리 이상으로 작성해주세요. ({pw_cnt}/3)  : ")
