1317c60c4fd1099c0aec3736e45580d30dc3eeb5
25th-Night <brown_eyed87@naver.com>
2023-07-05 23:30:20 +0900

    feat) Books 모델 - post 메서드 구현

--- books/models.py
+++ books/models.py
@@ -62,58 +62,10 @@ class Books:
             books = DatabaseManager(self.table, query).fetch_all()
             return books
 
+    def post(self):
+        query = f"""
+        INSERT INTO books (title, author, publisher)
+        VALUES ('{self.title}', '{self.author}', '{self.publisher}');
+        """
+        DatabaseManager(self.table, query).execute_query()
 
-# 전체 책 데이터 조회
-print("전체 책 데이터 조회")
-books = Books()
-all_books = books.get()
-print(f"all_books: {all_books}")
-assert len(all_books) == 12, "조회한 전체 책 데이터 수가 일치하지 않습니다."
-
-print()
-
-# id 오름차순 기준 5개의 책 데이터 조회
-print("id 오름차순 기준 5개의 책 데이터 조회")
-multiple_books = books.get(5)
-print(f"multiple_books: {multiple_books}")
-assert [book[0] for book in multiple_books] == [1, 2, 3, 4, 5], "조회한 5개의 책 데이터가 잘못되었습니다."
-
-print()
-
-# 3번 id의 책 데이터 조회
-print("3번 id의 책 데이터 조회")
-id_filtered_book = books.get(id=3)
-print(f"3번 id의 책: {id_filtered_book}")
-assert id_filtered_book[0] == 3, "id로 조회한 책 데이터가 잘못되었습니다."
-
-print()
-
-# 제목에 '파이썬'이 들어가는 책 데이터 조회
-print("제목에 '파이썬'이 들어가는 책 데이터 조회")
-title_filtered_books = books.get(title="파이썬")
-print(f"제목에 '파이썬'이 들어가는 책: {title_filtered_books}")
-assert {'파이썬' in book[1] for book in title_filtered_books} == {True}, "제목으로 조회한 책 데이터가 잘못되었습니다."
-
-print()
-
-# '홍길동'님이 집필한 책 데이터 조회
-print("'홍길동'님이 집필한 책 데이터 조회")
-author_filtered_books = books.get(author="홍길동")
-print(f"홍길동 저자의 책: {author_filtered_books}")
-assert {'홍길동' in book[2] for book in author_filtered_books} == {True}, "저자로 조회한 책 데이터가 잘못되었습니다."
-
-print()
-
-# 출판사명에 "한빛'이 들어가는 책 데이터 조횣
-print("출판사명에 '한빛'이 들어가는 책 데이터 조회")
-publisher_filtered_books = books.get(publisher="한빛")
-print(f"한빛 출판사의 책: {publisher_filtered_books}")
-assert {'한빛' in book[3] for book in publisher_filtered_books} == {True}, " 출판사명으로 조회한 책 데이터가 잘못되었습니다."
-
-print()
-
-# 대여 가능한 책 데이터 조회
-print("대여 가능한 책 데이터 조회")
-available_books = books.get(is_available=True)
-print(f"대여 가능한 책: {available_books}")
-assert {book[4] == True for book in available_books} == {True}, "대여 가능으로 조회한 책 데이터가 잘못되었습니다."
