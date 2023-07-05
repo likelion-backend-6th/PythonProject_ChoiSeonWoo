3a1e436ac02c63f34c2ccb9a01f417a29f925fb5
25th-Night <brown_eyed87@naver.com>
2023-07-05 22:45:58 +0900

    feat) Books 모델 - get 메서드 구현

--- books/models.py
+++ books/models.py
@@ -17,3 +17,47 @@ class Books:
         self.author = author
         self.publisher = publisher
         self.is_available = is_available
+
+    def get(
+            self,
+            size: Optional[int] = None,
+            id: Optional[int] = None,
+            title: Optional[str] = None,
+            author: Optional[str] = None,
+            publisher: Optional[str] = None,
+            is_available: Optional[bool] = None,
+    ) -> object:
+        if id is not None:
+            query = f"SELECT * FROM books WHERE id = '{id}';"
+            book = DatabaseManager(self.table, query).fetch_one()
+            return book
+
+        elif title is not None:
+            query = f"SELECT * FROM books WHERE title LIKE '%{title}%';"
+            books = DatabaseManager(self.table, query).fetch_all()
+            return books
+
+        elif author is not None:
+            query = f"SELECT * FROM books WHERE author = '{author}';"
+            books = DatabaseManager(self.table, query).fetch_all()
+            return books
+
+        elif publisher is not None:
+            query = f"SELECT * FROM books WHERE publisher LIKE '%{publisher}%';"
+            books = DatabaseManager(self.table, query).fetch_all()
+            return books
+
+        elif is_available is not None:
+            query = f"SELECT * FROM books WHERE is_available = '{is_available}';"
+            books = DatabaseManager(self.table, query).fetch_all()
+            return books
+
+        elif size is not None:
+            query = f"SELECT * from books order by id limit {size};"
+            books = DatabaseManager(self.table, query).fetch_many(size)
+            return books
+
+        else:
+            query = f"SELECT * FROM books;"
+            books = DatabaseManager(self.table, query).fetch_all()
+            return books
