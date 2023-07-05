23687800db9c8084370028601f3ec82d1a2cde5c
25th-Night <brown_eyed87@naver.com>
2023-07-05 20:43:42 +0900

    refactor) User 모델 - DatabaseManager 클래스의 수정 사항 반영

--- users/models.py
+++ users/models.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from common.database import DatabaseManager, postgres, PostgreSQL
+from common.database import DatabaseManager
 
 
 class Users:
@@ -17,15 +17,15 @@ class Users:
     def get(self, size: Optional[int] = None):
         if self.username != None:
             query = f"SELECT * from users WHERE username = '{self.username}';"
-            user = DatabaseManager(postgres, self.table, query).fetch_one()
+            user = DatabaseManager(self.table, query).fetch_one()
             return user
         elif size:
             query = f"SELECT * from users order by id limit {size};"
-            users = DatabaseManager(postgres, self.table, query).fetch_many(size)
+            users = DatabaseManager(self.table, query).fetch_many(size)
             return users
         else:
             query = "SELECT * from users order by id;"
-            users = DatabaseManager(postgres, self.table, query).fetch_all()
+            users = DatabaseManager(self.table, query).fetch_all()
             return users
 
     def post(self):
@@ -33,7 +33,7 @@ class Users:
         INSERT INTO users (username, fullname, password)
         VALUES ('{self.username}', '{self.fullname}', '{self.password}')
         """
-        DatabaseManager(postgres, self.table, query).execute_query()
+        DatabaseManager(self.table, query).execute_query()
 
     def put(self, new_fullname: str, new_password: Optional[str] = None):
         if new_password:
@@ -46,21 +46,27 @@ class Users:
             UPDATE users SET fullname = '{new_fullname}'
             WHERE username = '{self.username}';
             """
-        DatabaseManager(postgres, self.table, query).execute_query()
+        DatabaseManager(self.table, query).execute_query()
 
 
-# 모든 유저 조회
-users = Users()
-all_users = users.get()
-print(f"all_users: {all_users}")
-
-# 일부 유저 조회
-users = Users()
-multiple_users = users.get(3)
-print(f"multiple_users: {multiple_users}")
-
-
-# 특정 유저 조회
-users = Users("admin")
-single_user = users.get()
-print(f"single_user: {single_user}")
+# # 모든 유저 조회
+# print("fetch_all 메서드")
+# users = Users()
+# all_users = users.get()
+# print(f"all_users: {all_users}")
+#
+# print("")
+#
+# # 일부 유저 조회
+# print("fetch_many 메서드")
+# users = Users()
+# multiple_users = users.get(3)
+# print(f"multiple_users: {multiple_users}")
+#
+# print("")
+#
+# # 특정 유저 조회
+# print("fetch_one 메서드")
+# users = Users("admin")
+# single_user = users.get()
+# print(f"single_user: {single_user}")
