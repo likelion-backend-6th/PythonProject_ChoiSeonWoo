fa908142825dbf15e3aa3d103633cdbd41f775a3
25th-Night <brown_eyed87@naver.com>
2023-07-09 09:23:37 +0900

    update) books/utils.py - 도서 조회/검색 함수에 유효성 검사 함수 적용, 재귀 호출을 while 루프로 수정

--- books/utils.py
+++ books/utils.py
@@ -3,8 +3,8 @@ from time import sleep
 from typing import List, Union
 
 from books.models import Books, Loans
-from books.validation import fetch_type_validation, search_type_validation, \
-                             loan_book_ids_validation, return_book_ids_validation
+from books.validation import FETCH_TYPE_MESSAGE, SEARCH_TYPE_MESSAGE, type_validation, search_validation, \
+    loan_book_ids_validation, return_book_ids_validation
 
 
 def change_isavailable(books_list: List) -> List:
@@ -29,16 +29,11 @@ def restore_isavailable(books_list: List) -> List:
     return result
 
 
-def fetch_books_list() -> List:
-    message = "\n   희망하시는 조회 대상 도서 정보의 번호를 입력해주세요.\n" \
-              "   [  1. 모든 도서  2. 현재 대출 가능한 도서  ]\n" \
-              "   -->  번호 입력  :  "
-
-    fetch_type = int(input(message))
-    fetch_type = fetch_type_validation(fetch_type)
-
-    if not fetch_type:
-        fetch_books_list()
+def fetch_books_list() -> Union[List, str]:
+    while True:
+        fetch_type = type_validation(FETCH_TYPE_MESSAGE)
+        if fetch_type:
+            break
 
     if fetch_type == 1:
         books_list: List = Books().get()
@@ -48,32 +43,15 @@ def fetch_books_list() -> List:
         return change_isavailable(books_list) if books_list else "현재 모든 도서가 대출중입니다."
 
 
-def search_book_list(book_lists) -> Union[List, str]:
-    message1 = "\n   검색을 희망하는 항목에 대한 번호를 입력해주세요.\n" \
-               "   [  1. ID  2. 제목  ]\n" \
-               "   -->  번호 입력  :  "
+def search_book_list(book_list: List) -> List:
+    while True:
+        search_type = type_validation(SEARCH_TYPE_MESSAGE)
+        if search_type:
+            break
 
-    search_type_list = {1: "ID", 2: "제목"}
+    book_list = search_validation(search_type, book_list)
 
-    search_type = int(input(message1))
-    search_type = search_type_validation(search_type)
-
-    if not search_type:
-        search_book_list(book_lists)
-
-    message2 = f"\n   도서의 {search_type_list[search_type]} 을/를 입력해주세요.\n" \
-               f"   -->  {search_type_list[search_type]} 입력  :  "
-
-    target = input(message2)
-
-    if search_type == 1:
-        result: List = list(filter(lambda x: x[0] == int(target), book_lists))
-    elif search_type == 2:
-        result: List = list(filter(lambda x: target in x[1], book_lists))
-
-    result = result if result else f"해당 {search_type_list[search_type]} (으)로 검색한 도서는 존재하지 않습니다."
-
-    return result
+    return change_isavailable(book_list)
 
 
 def loan_books(user_id: int) -> List:
