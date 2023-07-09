01ef9f3ed8025718dba650e6aadc5e39dfb72317
25th-Night <brown_eyed87@naver.com>
2023-07-09 15:02:55 +0900

    update) users/validation.py - 유효성 검증 함수 개선

--- users/validation.py
+++ users/validation.py
@@ -5,72 +5,106 @@ from time import sleep
 from users.models import Users
 
 
-def username_validation(username: str, users: List):
-    usr_cnt = 0
+def username_validation():
+    init_message = "\n   가입에 사용할 사용자명을 입력해주세요.\n" \
+                   "   이미 가입된 회원의 경우, 로그인 메뉴로 이동하시려면 숫자 8을 입력해주세요.\n" \
+                   "   --->  입력  :  "
+    cnt = 0
+    message = init_message
+    users = Users().get()
+
     while True:
+        username = input(message).replace(" ","")
         username_check = map(lambda x: x[1] == username, users)
-        if True not in username_check:
-            return username  # 중복 검사 통과
-        usr_cnt += 1
-        if usr_cnt == 3:
-            print("3회 이상 실패하였으므로 초기 메뉴로 돌아갑니다.")
+        if username == "8" or True not in username_check:
+            return username
+
+        cnt += 1
+        message = f"\n   이미 가입된 정보가 있습니다. ({cnt}/3)\n"\
+                   "   확인 후 사용자명을 다시 입력해주세요.\n" \
+                   "   -->  사용자명 입력  :  "
+        if cnt == 3:
+            message, cnt = init_message, 0
+            print("   3회 이상 실패하였으므로 초기 메뉴로 돌아갑니다.")
             sleep(0.5)
             for i in range(3):
-                print(3 - i)
+                print(f"   {3 - i}")
                 sleep(0.5)
-            return False  # 중복 검사 실패
-        username = input(f"이미 가입된 정보가 있습니다. ({usr_cnt}/3)  사용자명을 다시 입력해주세요. : ")
 
 
-def password_validation(password: str):
-    pw_cnt = 0
-
+def password_validation():
+    init_message = "\n   가입에 사용할 비밀번호를 입력해주세요.\n" \
+                   "   --->  비밀번호 입력  :  "
+    cnt = 0
+    message = init_message
     match = "^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$"
     validation = re.compile(match)
 
     while True:
+        password = input(message)
         if validation.match(password) is not None:
             return password
-        pw_cnt += 1
-        if pw_cnt == 3:
+
+        cnt += 1
+        message = f"\n   비밀번호는 하나 이상의 문자, 숫자, 특수문자를 포함하여 8자리 이상으로 작성해주세요. ({cnt}/3)\n" \
+                   "   확인 후 비밀번호를 다시 입력해주세요.\n" \
+                   "   -->  비밀번호 입력  :  "
+        if cnt == 3:
+            message, cnt = init_message, 0
             print("3회 이상 실패하였으므로 초기 메뉴로 돌아갑니다.")
             sleep(0.5)
             for i in range(3):
-                print(3 - i)
+                print(f"   {3 - i}")
                 sleep(0.5)
-            return False
-        password = input(f"비밀번호는 하나 이상의 문자, 숫자, 특수문자를 포함하여 8자리 이상으로 작성해주세요. ({pw_cnt}/3)  : ")
 
 
-def user_validation(username: str):
-    usr_cnt = 0
+def user_validation():
+    init_message = "   사용자명을 입력해주세요.\n" \
+                   "   회원가입을 진행하시려면 숫자 7을 입력해주세요.\n" \
+                   "   --->  입력  :  "
+    cnt = 0
+    message = init_message
+
     while True:
-        user = Users(username).get()
+        username = input(message)
+        if username == "7":
+            return username
+        user = Users().get(username=username)
         if user:
             return user
-        usr_cnt += 1
-        if usr_cnt == 3:
+
+        cnt += 1
+        message = f"\n   조회되는 유저 정보가 없습니다. ({cnt}/3)\n" \
+                   "   확인 후 사용자명을 다시 입력해주세요.\n" \
+                   "   -->  사용자명 입력  :  "
+
+        if cnt == 3:
+            message, cnt = init_message, 0
             print("3회 이상 실패하였으므로 초기 메뉴로 돌아갑니다.")
             sleep(0.5)
             for i in range(3):
                 print(3 - i)
                 sleep(0.5)
-            return False
-        username = input(f"조회되는 정보가 없습니다. ({usr_cnt}/3)  사용자명을 다시 입력해주세요. : ")
 
 
-def password_validation2(password:str, user: Users):
-    pw_cnt = 0
+def password_validation2(user: List):
+    init_message = "   비밀번호를 입력해주세요.\n" \
+                   "   --->  입력  :  "
+    cnt = 0
+    message = init_message
+
     while True:
-        matching = (user[-1] == password)
-        if matching:
+        password = input(message)
+        if user[0][-1] == password:
             return True
-        pw_cnt += 1
-        if pw_cnt == 3:
+        cnt += 1
+        message = f"\n   비밀번호가 일치하지 않습니다. ({cnt}/3)\n" \
+                   "   확인 후 다시 입력해주세요.\n" \
+                   "   -->  비밀번호 입력  :  "
+
+        if cnt == 3:
             print("3회 이상 실패하였으므로 초기 메뉴로 돌아갑니다.")
             sleep(0.5)
             for i in range(3):
                 print(3 - i)
                 sleep(0.5)
-            return False
-        password = input(f"비밀번호가 일치하지 않습니다. ({pw_cnt}/3)  다시 입력해주세요. : ")
