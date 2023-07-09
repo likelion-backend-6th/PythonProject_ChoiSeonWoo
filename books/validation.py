b6db0ab45b4d1768ec6bad4219558e68ca4c3a44
25th-Night <brown_eyed87@naver.com>
2023-07-09 09:15:43 +0900

    update) validation.py - 도서검색/조회시, 유형 관련 입력값에 대한 유효성 검사 함수를 하나로 수정

--- books/validation.py
+++ books/validation.py
@@ -4,49 +4,47 @@ from typing import List
 from books.models import Books
 
 
-def fetch_type_validation(fetch_type: int):
-    cnt = 0
-    while True:
-        if fetch_type in [1, 2]:
-            return fetch_type
-        cnt += 1
-        if cnt == 3:
-            print("   3회 이상 실패하였으므로 초기 메뉴로 돌아갑니다.")
-            sleep(0.5)
-            for i in range(3):
-                print(f"   {3 - i}")
-                sleep(0.3)
-            return False
-        sleep(0.2)
-        fetch_type = int(input(f"   잘못 입력하였습니다. 번호를 다시 입력해주세요. ({cnt}/3)  :  "))
+FETCH_TYPE_MESSAGE = "\n   희망하시는 조회 대상 도서 정보의 번호를 입력해주세요.\n" \
+                     "   [  1. 모든 도서  2. 현재 대출 가능한 도서  ]\n" \
+                     "   -->  번호 입력  :  "
+
+SEARCH_TYPE_MESSAGE = "\n   검색을 희망하는 항목에 대한 번호를 입력해주세요.\n" \
+               "   [  1. ID  2. 제목  ]\n" \
+               "   -->  번호 입력  :  "
 
 
-def search_type_validation(search_type: int):
+def type_validation(message) -> int:
     cnt = 0
-    while True:
-        if search_type in [1, 2]:
-            return search_type
-        cnt += 1
-        if cnt == 3:
-            print("   3회 이상 실패하였으므로 초기 메뉴로 돌아갑니다.")
-            sleep(0.5)
-            for i in range(3):
-                print(f"   {3 - i}")
-                sleep(0.3)
-            return False
-        sleep(0.2)
-        search_type = int(input(f"   잘못 입력하였습니다. 번호를 다시 입력해주세요. ({cnt}/3)  :  "))
+
+    while cnt < 3:
+        try:
+            type_ = int(input(message))
+            if type_ in [1, 2]:
+                return type_
+            else:
+                cnt += 1
+                print(f"   잘못 입력하였습니다. 번호를 다시 입력해주세요. ({cnt}/3)")
+        except ValueError:
+            cnt += 1
+            print(f"   잘못 입력하였습니다. 숫자를 입력해주세요. ({cnt}/3)")
+
+    print("   3회 이상 실패하였으므로 초기 메뉴로 돌아갑니다.")
+    sleep(0.5)
+    for i in range(3):
+        print(f"   {3 - i}")
+        sleep(0.3)
+    return False
 
 
 def loan_book_ids_validation() -> List:
     loanable_book_id_list = list(map(lambda x: x[0], Books().get(is_available=True)))
     cnt = 0
 
-    message3 = "\n   대출을 희망하는 도서의 ID를 입력해주세요.\n" \
+    message = "\n   대출을 희망하는 도서의 ID를 입력해주세요.\n" \
                "   여러 권을 대출하고자 하는 경우, 쉼표(,)로 구분하여 ID를 입력해주세요.\n" \
                "   -->  ID 입력  :  "
 
-    book_ids = input(message3).replace(" ", "")
+    book_ids = input(message).replace(" ", "")
     if not book_ids:
         return []
     book_id_list = list(map(int, book_ids.split(",")))
@@ -69,10 +67,10 @@ def loan_book_ids_validation() -> List:
             return loan_book_ids_validation()
         sleep(0.2)
 
-        message3 = error_message + f"ID를 다시 입력해주세요. ({cnt}/3)  :  \n" \
+        message = error_message + f"ID를 다시 입력해주세요. ({cnt}/3)  :  \n" \
                    "   여러 권을 대출하고자 하는 경우, 쉼표(,)로 구분하여 ID를 입력해주세요.\n" \
                    "   -->  ID 입력  :  "
-        book_ids = input(message3)
+        book_ids = input(message)
         book_id_list = book_ids.replace(" ", "").split(",")
         book_id_list = list(map(int, book_id_list))
 
@@ -80,7 +78,7 @@ def loan_book_ids_validation() -> List:
 def return_book_ids_validation(user_id) -> List:
     returnable_book_id_list = list(map(lambda x: x[0], Books().get(is_available=False, user_id=user_id)))
     cnt = 0
-    print(f"returnable_book_id_list: {returnable_book_id_list}")
+
     message4 = "\n   반납을 희망하는 도서의 ID를 입력해주세요.\n" \
                "   여러 권을 반납하고자 하는 경우, 쉼표(,)로 구분하여 ID를 입력해주세요.\n" \
                "   -->  ID 입력  :  "
