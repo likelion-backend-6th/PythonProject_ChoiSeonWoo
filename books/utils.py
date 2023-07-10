69c166ee57103c1020e248acb5e63adecc67307d
25th-Night <brown_eyed87@naver.com>
2023-07-11 08:30:55 +0900

    feat) books/utils.py - 다수 기능 수정 및 추가
    1. change_book_list
    - 대여가능 유무에 따른 대출일/반납일 컬럼 데이터 형식 변경
    - 도서 데이터 형태에 따른 컬럼 개수 뱐경
    
    2. change_loan_list
    - 대출일/반납일 컬럼 데이터 형식 변경
    
    3. render_table
    - tabulate 라이브러를 이용한 데이터 출력 형태 변경
    - 도서 데이터 출력하는 코드에 대해 모두 적용
    
    4. fetch_book_list
    - 모든 도서가 대출중일 경우 메세지 출력
    
    5. loan_books
    - 대출 함수 실행 시, 현재 대출 가능한 도서 목록 조회 여부를 묻는 기능 추가
    - 도서 대출 완료 시, 유저의 현재 대출 도서 목록을 출력
    
    5. loan_books
    - 대출 함수 실행 시, 유저의 현재 대출 도서 목록을 출력
    - 도서 대출 완료 시, 갱신된 유저의 현재 대출 도서 목록을 출력

--- books/utils.py
+++ books/utils.py
@@ -2,51 +2,117 @@ from datetime import datetime
 from time import sleep
 from typing import List, Union, Optional
 
+from tabulate import tabulate
+
 from books.models import Books, Loans
 from books.validation import FETCH_TYPE_MESSAGE, SEARCH_TYPE_MESSAGE, type_validation, search_validation, \
     loan_book_ids_validation, return_book_ids_validation
+from common.validation import bool_validation, SEARCH_LOANABLE_MESSAGE
+from users.models import Users
 
 
-def change_isavailable(books_list: List) -> List:
+def change_book_list(book_list: List, func_type: int) -> List:
+    func_type_info = {1: "도서 정보", 2: " 대출일자 포함", 3: "전체"}
+
     result = []
-    for book in books_list:
-        if type(book[4] == bool):
+    if not book_list:
+            book_list = [[" - "] * 6]
+
+    for book in book_list:
+        if type(book[4]) == bool:
             if book[4]:
-                book = book[:4] + ('대여가능',) + ('', '')
+                book = list(book[:4]) + ["대여가능", " - ", " - "]
             elif not book[4]:
-                loan_date = book[5].strftime("%Y년 %m월 %d일") if book[5] else ''
-                return_date = book[6].strftime("%Y년 %m월 %d일") if book[6] else ''
-                book = book[:4] + ('대여중',) + (loan_date, return_date)
+                loan_date = book[5].strftime("%Y년 %m월 %d일") if book[5] else ' - '
+                return_date = book[6].strftime("%Y년 %m월 %d일") if book[6] else ' - '
+                book = list(book[:4]) + ["대여중", loan_date, return_date]
 
-            result.append(book)
+        if func_type == 1:
+            book = book[:4]
+        elif func_type == 2:
+            book = book[:6]
+        elif func_type == 3:
+            book = book
 
-    return result if result else books_list
+        result.append(book)
+
+    return result if result else book_list
 
 
-def fetch_books_list() -> List | str | bool:
-    print("\n   =========           도서 조회를 진행합니다.          =========\n")
+def change_loan_list(loan_list: List) -> List:
+    result = []
+    if not loan_list:
+        loan_list = [[" - "] * 5]
+
+    for loan in loan_list:
+        if type(loan[3] == datetime):
+            loan_date = loan[3].strftime("%Y년 %m월 %d일 %H시 %M분")
+            return_date = loan[4].strftime("%Y년 %m월 %d일") if loan[4] else ''
+            loan = list(loan[:3]) + [loan_date, return_date]
+
+            result.append(loan)
+
+    return result if result else loan_list
+
+
+def render_table(data, table_name):
+    headers = []
+    if table_name =="users":
+        headers = ["No.", "ID", "사용자명", "성함"]
+    elif table_name =="books":
+        headers = ["NO.", "ID", "제목", "저자", "출판사", "대출가능여부", "대출일"]
+        headers = headers[:len(data[0])+1]
+    elif table_name == "loans":
+        headers = ["NO.", "ID", "사용자 ID", "도서 ID", "대출일", "반납일"]
+
+    table = tabulate(data, headers=headers, tablefmt="fancy_grid",
+                     showindex=True, numalign='center', stralign='left', maxcolwidths=30)
+    table_lines = table.split("\n")
+    table_lines_modified = ["   " + line for line in table_lines]
+    table = "\n".join(table_lines_modified)
+
+    return table
+
+
+def fetch_book_list() -> List | int:
+    print("\n   =========           도서 조회를 진행합니다.          =========")
     fetch_type = type_validation(FETCH_TYPE_MESSAGE)
 
-    if fetch_type == 1:
-        books_list: List = Books().get()
-        return change_isavailable(books_list)
+    if fetch_type == -1:
+        return -1
+
+    elif fetch_type == 1:
+        book_list: List = Books().get()
 
     elif fetch_type == 2:
-        books_list: List = Books().get(is_available=True)
+        book_list: List = Books().get(is_available=True)
 
-        return change_isavailable(books_list) if books_list else "현재 모든 도서가 대출중입니다."
-    elif fetch_type == -1:
-        return -1
+    if not book_list:
+        print("\n   현재 모든 도서가 대출중입니다.\n")
+
+    func_type = 2 if fetch_type == 1 else 1
+    print("\n   [조회 결과]")
+    print(render_table(change_book_list(book_list, func_type), "books"))
+
+    return [book_list, func_type] if book_list else -1
 
 
-def search_book_list(book_list: Optional[List] = None) -> List | bool:
+def search_book_list(book_list: Optional[List] = None) -> List | int:
     print("\n   =========           도서 검색를 진행합니다.          =========\n")
-    book_list = book_list if book_list else fetch_books_list()
 
-    if book_list == -1:
-        return -1
-
-    print("\n   지정하신 도서를 대상으로 검색을 진행합니다.")
+    if book_list:
+        book_list, func_type = book_list, 2
+        render_table_title = "\n   [대상 도서]"
+        print(render_table_title)
+        print(render_table(change_book_list(book_list, func_type), "books"))
+    else:
+        book_check = bool_validation(SEARCH_LOANABLE_MESSAGE)
+        if book_check:
+            book_list = Books().get(is_available=True)
+            print("\n   현재 대출 가능한 도서 목록입니다.")
+            print(render_table(change_book_list(book_list, 1), 'books'))
+        elif book_check is None:
+            return -1
 
     search_type = type_validation(SEARCH_TYPE_MESSAGE)
     if search_type == -1:
@@ -54,15 +120,26 @@ def search_book_list(book_list: Optional[List] = None) -> List | bool:
 
     book_list = search_validation(search_type, book_list)
 
-    return change_isavailable(book_list) if book_list != -1 else False
+    print("\n   [검색 결과]")
+    print(render_table(change_book_list(book_list, func_type), "books"))
+
+    return book_list if book_list != -1 else -1
 
 
-def loan_books(user_id: int) -> List | bool:
+def loan_books(user_id: int) -> List | int:
     print("\n   =========           도서 대여를 진행합니다.          =========\n")
+
+    book_check = bool_validation(SEARCH_LOANABLE_MESSAGE)
+    if book_check:
+        print("\n   현재 대출 가능한 도서 목록입니다.")
+        print(render_table(change_book_list(Books().get(is_available=True), 1), 'books'))
+    elif book_check is None:
+        return -1
+
     book_id_list = loan_book_ids_validation()
 
     if book_id_list == -1:
-        return False
+        return -1
 
     elif not book_id_list:
         return []
@@ -72,22 +149,44 @@ def loan_books(user_id: int) -> List | bool:
             target_book = Books().put(id=book_id)
             new_loan = Loans(user_id, book_id).post()
 
-    return change_isavailable(Books().get(user_id=user_id, is_available=False, order_by_info=('l.loan_date', "DESC")))
+    current_loan_books = Books().get(user_id=user_id, is_available=False, order_by_info=('l.loan_date', "DESC"))
+
+    print("\n   대출이 정상적으로 완료되었습니다.")
+
+    print("\n   현재 대출 중인 도서의 목록입니다.")
+    print(render_table(change_book_list(current_loan_books, 2), 'books'))
+
+    return current_loan_books
 
 
-def return_books(user_id: int) -> List | bool:
+def return_books(user_id: int) -> List | int:
     print("\n   =========           도서 반납을 진행합니다.          =========\n")
-    book_id_list = return_book_ids_validation(user_id)
+
+    retunable_book_list = Books().get(user_id=user_id, is_available=False, order_by_info=('l.loan_date', "DESC"))
+    username = Users().get(id=user_id)[0][1]
+
+    if not retunable_book_list:
+        print(f"   {username} 님의 반납 가능한 도서가 존재하지 않습니다.\n")
+        return -1
+
+    print(f"\n   {username} 님의 반납 가능한 도서 목록입니다.")
+    print(render_table(change_book_list(retunable_book_list, 2), 'books'))
+
+    book_id_list = return_book_ids_validation(user_id, retunable_book_list)
 
     if book_id_list == -1:
-        return False
-
-    elif not book_id_list:
-        return []
+        return -1
 
     else:
         for book_id in book_id_list:
             target_book = Books().put(id=book_id)
             update_return = Loans().put(return_date=datetime.now(), return_update=True, return_book_id=book_id)
 
-    return change_isavailable(Books().get(user_id=user_id, is_available=False, order_by_info=('l.loan_date', "DESC")))
+    current_loan_books = Books().get(user_id=user_id, is_available=False, order_by_info=('l.loan_date', "DESC"))
+
+    print("\n   반납이 정상적으로 완료되었습니다.")
+
+    print("\n   현재 대출 중인 도서의 목록입니다.")
+    print(render_table(change_book_list(current_loan_books, 2), 'books'))
+
+    return current_loan_books
