e5a0e02f18366cce27287f0838785292038c955b
25th-Night <brown_eyed87@naver.com>
2023-07-10 21:41:14 +0900

    feat) common/utils.py - 시스템 대기 함수 구현

--- common/utils.py
+++ common/utils.py
@@ -1,7 +1,10 @@
+import sys
 from typing import Iterable, List
 
 import pandas as pd
 from pandas import DataFrame
+
+from common.validation import stand_by_validation
 
 
 def iter_to_df(iterable_data: Iterable, column_names: List) -> DataFrame:
@@ -9,3 +12,12 @@ def iter_to_df(iterable_data: Iterable, column_names: List) -> DataFrame:
     df.index = range(1, len(df) + 1)
     return df
 
+
+def stand_by():
+    while True:
+        print("\n   =======            시스템 대기 화면            =======")
+        is_stood_by = stand_by_validation()
+        if is_stood_by == 1:
+            return -1
+        if is_stood_by == 3:
+            sys.exit()
