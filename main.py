ddb3ee7ff0c94d709606b170f0fc142a9f4362d4
25th-Night <brown_eyed87@naver.com>
2023-07-10 18:43:23 +0900

    update) main.py - manage_user 메서드 코드 개선

--- main.py
+++ main.py
@@ -8,6 +8,17 @@ from users.utils import sign_up, login, logout
 
 
 class LibrarySystem:
+    MENU_INFO: Dict[str, str] = {
+        "1": "도서 조회",
+        "2": "도서 검색",
+        "3": "도서 대출",
+        "4": "도서 반납",
+        "7": "회원 가입",
+        "8": "로그인",
+        "9": "로그아웃",
+        "-1": "이전 메뉴",
+        "007": "프로그램 종료",
+    }
 
     MENU: Dict[str, Callable] = {
         "1": fetch_books_list,
@@ -21,23 +32,38 @@ class LibrarySystem:
     }
 
     MESSAGE: Dict[str, str] = {
-        "welcome": "=== 안녕하세요. 오늘도 도서관리시스템을 이용해주셔서 감사합니다. ===\n"
-                   "===========     지금부터 시스템을 시작하겠습니다.      ==========",
+        "welcome": "\n   === 안녕하세요. 오늘도 도서관리시스템을 이용해주셔서 감사합니다. ===\n"
+                   "   ============     지금부터 시스템을 시작하겠습니다.      ============",
+        "manage_user": "\n   ===========         사용자 관리 메뉴입니다.         ===========",
+        "manage_book": "\n   ===========          도서 관리 메뉴입니다.          ===========",
+        "bye": "\n   =======  오늘도 저희 도서관리시스템을 이용해주셔서 감사합니다. ========\n"
+               "   =============           다음에 다시 이용해주세요.          ============",
     }
 
-    progress: List= []
-    user: Optional[Dict] = None
+    def __init__(self):
+        self.user: List = []
 
     def manage_user(self):
-        execute_menu_num = menu_num_validation(USER_MENU_NUM_LIST, USER_MENU_INIT_MESSAGE, self.progress)
-        self.user = self.MENU[execute_menu_num](self.progress)
+        print(self.MESSAGE["manage_user"])
+        while not self.user:
+            execute_menu_num = menu_num_validation(USER_MENU_NUM_LIST, USER_MENU_INIT_MESSAGE)
+            result = self.MENU[execute_menu_num]()
+
+            if result == -1:
+                continue
+            else:
+                self.user = result
+                break
 
     def manage_book(self):
         pass
 
     def main(self):
+        print(self.MESSAGE["welcome"])
         create_table(TABLES, CREATE_QUERY_LISTS)
-        self.manage_user()
+        while True:
+            if not self.user:
+                self.manage_user()
 
 
 if __name__ == "__main__":
