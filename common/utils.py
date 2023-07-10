3af02e0841ddbab314c9bb4d61f84243409389f2
25th-Night <brown_eyed87@naver.com>
2023-07-10 22:31:52 +0900

    refactor) create_table 함수를 utils.py 로 이동

--- common/utils.py
+++ common/utils.py
@@ -4,7 +4,13 @@ from typing import Iterable, List
 import pandas as pd
 from pandas import DataFrame
 
+from common.database import DatabaseManager
 from common.validation import stand_by_validation
+
+
+def create_table(tables: List, queries: List):
+    for table, query in zip(tables, queries):
+        DatabaseManager(table, query).execute_query()
 
 
 def iter_to_df(iterable_data: Iterable, column_names: List) -> DataFrame:
