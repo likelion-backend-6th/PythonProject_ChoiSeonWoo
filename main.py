d2264ed396b0410bdad0f0e866f5f1e9160f2fdc
25th-Night <brown_eyed87@naver.com>
2023-07-10 21:38:52 +0900

    feat) main.py - 시스템 종료 함수 구현

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from typing import List, Dict, Callable
 from books.utils import fetch_books_list, search_book_list, loan_books, return_books
 from common.database import create_table
 from common.settings import TABLES, CREATE_QUERY_LISTS
+from common.utils import stand_by
 from common.validation import menu_num_validation, USER_MENU_NUM_LIST, USER_MENU_INIT_MESSAGE, \
-                              BOOK_MENU_NUM_LIST, BOOK_MENU_INIT_MESSAGE
+    BOOK_MENU_NUM_LIST, BOOK_MENU_INIT_MESSAGE, bool_validation, TERMINATE_MESSAGE
 from users.utils import sign_up, login, logout
 
 
@@ -29,7 +30,6 @@ class LibrarySystem:
         "7": sign_up,
         "8": login,
         "9": logout,
-        "007": None,
     }
 
     MESSAGE: Dict[str, str] = {
@@ -39,6 +39,8 @@ class LibrarySystem:
         "manage_book": "\n   ===========          도서 관리 메뉴입니다.          ===========",
         "bye": "\n   =======  오늘도 저희 도서관리시스템을 이용해주셔서 감사합니다. ========\n"
                "   =============           다음에 다시 이용해주세요.          ============",
+        "terminate": "\n   ===========         시스템 종료 안내         ==========="
+
     }
 
     def __init__(self):
@@ -48,7 +50,11 @@ class LibrarySystem:
         print(self.MESSAGE["manage_user"])
         while not self.user:
             execute_menu_num = menu_num_validation(USER_MENU_NUM_LIST, USER_MENU_INIT_MESSAGE)
-            result = self.MENU[execute_menu_num]()
+
+            if execute_menu_num == "007":
+                result = self.terminate()
+            else:
+                result = self.MENU[execute_menu_num]()
 
             if result == -1:
                 print(self.MESSAGE["manage_user"])
@@ -58,26 +64,45 @@ class LibrarySystem:
                 break
 
     def manage_book(self):
-        print(self.MESSAGE["manage_book"])
+
         while True:
+            print(self.MESSAGE["manage_book"])
             result = 0
             execute_menu_num = menu_num_validation(BOOK_MENU_NUM_LIST, BOOK_MENU_INIT_MESSAGE)
 
-            if execute_menu_num in ["1", "2"]:
+            if execute_menu_num == "007":
+                result = self.terminate()
+
+            elif execute_menu_num in ["1", "2"]:
                 result = self.MENU[execute_menu_num]()
             elif execute_menu_num in ["3", "4"]:
                 result = self.MENU[execute_menu_num](self.user[0][0])
+
             if result == -1:
                 break
+
             if execute_menu_num == "9":
                 current_user_info = self.MENU[execute_menu_num]()
+
                 if current_user_info is None:
                     self.user = current_user_info
                     break
 
+    def terminate(self):
+        print("\n   =======            시스템 종료 화면            =======\n")
+        is_stood_by = bool_validation(TERMINATE_MESSAGE)
+
+        if is_stood_by:
+            self.user = None
+
+            return stand_by()
+        else:
+            return -1
+
     def main(self):
         print(self.MESSAGE["welcome"])
         create_table(TABLES, CREATE_QUERY_LISTS)
+
         while True:
             if not self.user:
                 self.manage_user()
@@ -86,7 +111,6 @@ class LibrarySystem:
 
             if self.user:
                 continue
-            print(self.MESSAGE["bye"])
 
 
 if __name__ == "__main__":
