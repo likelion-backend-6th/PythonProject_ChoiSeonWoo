2dac960f13359b5a7b6bf032dd8ef77683331dc7
25th-Night <brown_eyed87@naver.com>
2023-07-11 20:53:23 +0900

    etc) common/utils.py - 미사용 함수 삭제

--- common/utils.py
+++ common/utils.py
@@ -1,8 +1,5 @@
 import sys
-from typing import Iterable, List
-
-import pandas as pd
-from pandas import DataFrame
+from typing import List
 from tabulate import tabulate
 
 from common.database import DatabaseManager
@@ -12,12 +9,6 @@ from common.validation import stand_by_validation
 def create_table(tables: List, queries: List):
     for table, query in zip(tables, queries):
         DatabaseManager(table, query).execute_query()
-
-
-def iter_to_df(iterable_data: Iterable, column_names: List) -> DataFrame:
-    df = pd.DataFrame(iterable_data, columns=column_names)
-    df.index = range(1, len(df) + 1)
-    return df
 
 
 def stand_by():
