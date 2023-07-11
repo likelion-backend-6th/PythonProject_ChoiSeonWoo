d5aee48026aceb2566222f5ba4aec1e729f094e4
25th-Night <brown_eyed87@naver.com>
2023-07-11 14:45:59 +0900

    feat) users/utils.py - 유저 정보 조회/등록/수정 기능 구현

--- users/utils.py
+++ users/utils.py
@@ -1,10 +1,11 @@
 from time import sleep
 from typing import List
 
-from common.validation import bool_validation, LOGOUT_MESSAGE
+from common.utils import render_table
+from common.validation import bool_validation, LOGOUT_MESSAGE, PASSWORD_MESSAGE
 from users.models import Users
 from users.validation import username_validation, password_validation, user_validation, password_validation2, \
-    fullname_validation
+    fullname_validation, user_id_validation
 
 
 def sign_up():
@@ -27,12 +28,10 @@ def sign_up():
     if password == "-1":
         return -1
 
-    print(username, fullname, password)
-
     new_user = Users(username, fullname, password)
     new_user.post()
 
-    print("\n   회원가입이 완료되었습니다.\n" 
+    print("\n   회원가입이 완료되었습니다.\n"
           "   이어서 로그인을 바로 진행하도록 하겠습니다.")
     sleep(0.5)
     for i in range(3):
@@ -71,3 +70,74 @@ def logout():
         return True
 
 
+def fetch_user():
+    print("\n   =======            모든 유저 정보를 조회합니다.            ========\n")
+
+    user = Users().get()
+
+    print(render_table(user, "users"))
+
+    return user
+
+def create_user():
+    print("\n   =========           유저 정보를 등록합니다.           =========")
+
+    username = username_validation()
+
+    if username == "8":
+        return login()
+    elif username == "-1":
+        return -1
+
+    fullname = fullname_validation()
+
+    if fullname == "-1":
+        return -1
+
+    password = password_validation()
+
+    if password == "-1":
+        return -1
+
+    new_user = Users(username, fullname, password)
+    new_user.post()
+
+
+    user = Users().get(username=username)
+
+    print("\n   유저 등록이 완료되었습니다.")
+    print(render_table(user, "users"))
+
+    return user
+
+
+def update_user():
+    print("\n   =========           유저 정보를 수정합니다.           =========")
+
+    user = user_id_validation()
+
+    if user == -1:
+        return -1
+
+    new_fullname = fullname_validation()
+
+    if new_fullname == "-1":
+        return -1
+
+    password_check = bool_validation(PASSWORD_MESSAGE)
+
+    if password_check is None:
+        return -1
+
+    elif password_check:
+        new_password = password_validation()
+        Users().put(id=user[0], fullname=new_fullname, password=new_password)
+    elif not password_check:
+        Users().put(id=user[0], fullname=new_fullname)
+
+    updated_user = Users().get(id=user[0])
+
+    print("\n   유저 정보 수정이 완료되었습니다.")
+    print(render_table(updated_user, "users"))
+
+    return user
