a653a65d5a58d9a3336455de31f983e09c2899af
25th-Night <brown_eyed87@naver.com>
2023-07-11 20:23:19 +0900

    update) common/utils.py - render_table 함수 수정

--- common/utils.py
+++ common/utils.py
@@ -33,12 +33,12 @@ def stand_by():
 def render_table(data, table_name):
     headers = []
     if table_name =="users":
-        headers = ["No.", "ID", "사용자명", "성함"]
+        headers = ["ID", "사용자명", "성함"]
     elif table_name =="books":
-        headers = ["NO.", "ID", "제목", "저자", "출판사", "대출가능여부", "대출일"]
+        headers = ["ID", "제목", "저자", "출판사", "대출가능여부", "대출일", "반납일"]
         headers = headers[:len(data[0])+1]
     elif table_name == "loans":
-        headers = ["NO.", "ID", "사용자 ID", "도서 ID", "대출일", "반납일"]
+        headers = ["ID", "사용자 ID", "도서 ID", "대출일", "반납일"]
 
     table = tabulate(data, headers=headers, tablefmt="fancy_grid",
                      showindex=True, numalign='center', stralign='left', maxcolwidths=30)
